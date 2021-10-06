d2c80b8be667cc80949a6ab5bf566a58433d13bc
Caroline Feldhaus <carolinefe@hotmail.com>
2021-10-06 10:34:19 -0300

    adicionando arquivo no github

---
+++ arrayappend.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+//arrays
+var copasdomundo : [Int] = [1958, 1962, 1970, 1994, 2002]
+
+var filmesquegosto : [String] = ["Toy Story", "O Senhor Dos Aneis", "Os Vingadores"]
+
+print(copasdomundo)
+
+//append vai incluir na array
+copasdomundo.append(2020)
+filmesquegosto.append("Harry Potter")
+
+print(copasdomundo)
+print(filmesquegosto)
